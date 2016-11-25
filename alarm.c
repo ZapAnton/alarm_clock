35f452e5fdf2d4311c40a5122377cba4179addaf
ZapAnton <wc3ft2@gmail.com>
2016-11-25 15:06:27 +0300

    Alarm loop now stops after certain time

--- alarm.c
+++ alarm.c
@@ -4,7 +4,14 @@
 int main(int argc, char** argv) {
 	PlaySound(TEXT("files\\alarm.wav"), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
 	
-	sleep(5000);
+	for (int i = 0; i < 5; ++i) {
+		printf("WAITING %d...\n", i);
+		Sleep(1000);
+	}
+		
+	if (!PlaySound(NULL, 0, 0)) {
+		puts("COULD NOT STOP SOUND");
+	}
 	
 	return 0;
 }
