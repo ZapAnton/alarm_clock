61b2768af5c29b32137fa962d6f1d9b5bf37ca2d
ZapAnton <wc3ft2@gmail.com>
2016-11-25 15:30:10 +0300

    Added stop_alarm function

--- alarm.c
+++ alarm.c
@@ -10,23 +10,33 @@
 
 void start_alarm(void);
 
+void stop_alarm(void);
+
 int main(int argc, char** argv) {
 	start_alarm();
 	
-	for (int i = 0; i < 5; ++i) {
-		printf("WAITING %d...\n", i);
-		Sleep(1000);
-	}
+	stop_alarm();
 		
-	if (!PlaySound(NULL, 0, 0)) {
-		puts("COULD NOT STOP SOUND");
-	}
-	
 	return 0;
 }
 
 void start_alarm(void) {
 	#ifdef _WIN32	
 	PlaySound(TEXT("files\\alarm.wav"), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
-	#endif	
+	#endif
+
+	for (int i = 0; i < 5; ++i) {
+		printf("WAITING %d...\n", i);
+		Sleep(1000);
+	}	
+}
+
+void stop_alarm(void) {
+	
+	
+	#ifdef _WIN32	
+	if (!PlaySound(NULL, 0, 0)) {
+		puts("COULD NOT STOP SOUND");
+	}	
+	#endif
 }
