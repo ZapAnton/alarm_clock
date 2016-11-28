80c44ce4c457a1e6cf3ae319ea4d6c1cc90330f2
ZapAnton <wc3ft2@gmail.com>
2016-11-28 10:06:02 +0300

    Added hours and minutes to command line arguments

--- alarm.c
+++ alarm.c
@@ -14,22 +14,30 @@ void start_alarm(void);
 
 void stop_alarm(void);
 
-void wait_before_alarm(int);
+void wait_before_alarm(int seconds_to_wait, int minutes_to_wait, int hours_to_wait);
 
 int main(int argc, char** argv) {
 	char input_param;
 	
 	int seconds_to_wait = 0;
+	int minutes_to_wait = 0;
+	int hours_to_wait = 0;
 	
-	while ((input_param = getopt(argc, argv, "s:")) != -1) {
+	while ((input_param = getopt(argc, argv, "h:m:s:")) != -1) {
 		switch (input_param) {
 			case 's':
 				seconds_to_wait = atoi(optarg);
 				break;
+			case 'm':
+				minutes_to_wait = atoi(optarg);
+				break;
+			case 'h':
+				hours_to_wait = atoi(optarg);
+				break;
 		}
 	}
 	
-	wait_before_alarm(seconds_to_wait);
+	wait_before_alarm(seconds_to_wait, minutes_to_wait, hours_to_wait);
 		
 	start_alarm();
 	
@@ -38,11 +46,16 @@ int main(int argc, char** argv) {
 	return 0;
 }
 
-void wait_before_alarm(int seconds_to_wait) {
-	printf("Alarm is set to start in %d seconds\n", seconds_to_wait);
+void wait_before_alarm(int seconds_to_wait, int minutes_to_wait, int hours_to_wait) {
+	printf(
+		"Alarm is set to start in %d hours %d minutes %d seconds\n", 
+		hours_to_wait, 
+		minutes_to_wait, 
+		seconds_to_wait
+	);
 	
 	#ifdef _WIN32
-	Sleep(seconds_to_wait * 1000);
+	Sleep(( (hours_to_wait * 3600) + (minutes_to_wait * 60) + seconds_to_wait) * 1000);
 	#endif
 }
 
@@ -50,7 +63,6 @@ void start_alarm(void) {
 	#ifdef _WIN32	
 	PlaySound(TEXT("files\\alarm.wav"), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
 	
-
 	for (int i = 0; i < 5; ++i) {
 		printf("WAITING %d...\n", i);
 		Sleep(1000);
