fb1a71b439971da4d0dc56e3c65e9fe58b686aa7
ZapAnton <wc3ft2@gmail.com>
2016-11-28 16:14:58 +0300

    Moved command line arguments parsing to separate function

--- alarm.c
+++ alarm.c
@@ -4,27 +4,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+void get_await_time(await_time* await_time, int argc, char** argv);
 
 void wait_before_alarm(await_time* await_time);
 
-int main(int argc, char** argv) {
-	char input_param;
-	
+int main(int argc, char** argv) {	
 	await_time await_time = {.seconds = 0, .minutes = 0, .hours = 0};
-		
-	while ((input_param = getopt(argc, argv, "h:m:s:")) != -1) {
-		switch (input_param) {
-			case 's':
-				await_time.seconds = atoi(optarg);
-				break;
-			case 'm':
-				await_time.minutes = atoi(optarg);
-				break;
-			case 'h':
-				await_time.hours = atoi(optarg);
-				break;
-		}
-	}
+	
+	get_await_time(&await_time, argc, argv);
 	
 	wait_before_alarm(&await_time);
 		
@@ -33,6 +20,24 @@ int main(int argc, char** argv) {
 	stop_alarm();
 
 	return 0;
+}
+
+void get_await_time(await_time* await_time, int argc, char** argv) {
+	char input_param;
+	
+	while ((input_param = getopt(argc, argv, "h:m:s:")) != -1) {
+		switch (input_param) {
+			case 's':
+				await_time->seconds = atoi(optarg);
+				break;
+			case 'm':
+				await_time->minutes = atoi(optarg);
+				break;
+			case 'h':
+				await_time->hours = atoi(optarg);
+				break;
+		}
+	}
 }
 
 void wait_before_alarm(await_time *await_time) {
