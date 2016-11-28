cfeeca46f5ae2f4318e5b73bd4300753e478c8b0
ZapAnton <wc3ft2@gmail.com>
2016-11-28 09:29:27 +0300

    Added Sleep in for loop to WIN32 #ifdef

--- alarm.c
+++ alarm.c
@@ -23,17 +23,17 @@ int main(int argc, char** argv) {
 void start_alarm(void) {
 	#ifdef _WIN32	
 	PlaySound(TEXT("files\\alarm.wav"), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
-	#endif
+	
 
 	for (int i = 0; i < 5; ++i) {
 		printf("WAITING %d...\n", i);
 		Sleep(1000);
 	}	
+	
+	#endif
 }
 
 void stop_alarm(void) {
-	
-	
 	#ifdef _WIN32	
 	if (!PlaySound(NULL, 0, 0)) {
 		puts("COULD NOT STOP SOUND");
